d38f165d6445c5ca84d8dee2071ed8742a01b183
Janko Marohnić <janko.marohnic@gmail.com>
2020-10-31 16:59:54 +0100

    Update rodauth-rails and sequel-activerecord_connection

--- Gemfile
+++ Gemfile
@@ -1,11 +1,10 @@
 source "https://rubygems.org"
-ruby "2.6.5"
 
 gem "rails", "~> 6.0"
 gem "pg", "~> 1.2"
 gem "puma", "~> 4.3"
 gem "webpacker", "~> 5.0"
 
-gem "rodauth-rails", "~> 0.2"
+gem "rodauth-rails", "~> 0.3"
 gem "rotp", require: false
 gem "rqrcode", require: false
--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,6 +1,4 @@
 require "sequel/core"
 
-# initialize the appropriate Sequel adapter without creating a connection
-DB = Sequel.postgres(test: false)
-# have Sequel use ActiveRecord's connection for database interaction
-DB.extension :activerecord_connection
+# initialize Sequel and have it reuse Active Record's database connection
+DB = Sequel.postgres(extension: :activerecord_connection)
