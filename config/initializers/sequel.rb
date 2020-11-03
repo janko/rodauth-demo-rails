3d480d0a0fe0e4fa4bc4baf193bbda2dd3ee3091
Adilson Carvalho <lc.adilson@gmail.com>
2020-11-03 16:10:17 +1100

    Bumping rodauth-rails version to 0.4.0 (#8)
    * Bumping rodauth-rails version to 0.4.0
    
    * Preventing database connection errors
    
    When creating a new database this code might fail. This was addressed as
    mentioned at https://github.com/janko/rodauth-rails/issues/14.

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,4 +1,4 @@
 require "sequel/core"
 
 # initialize Sequel and have it reuse Active Record's database connection
-DB = Sequel.postgres(extension: :activerecord_connection)
+DB = Sequel.postgres(extension: :activerecord_connection, test: false)
