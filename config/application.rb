6ad80d8d1408334504f8b0b8860f3248b6a6b9b4
Janko Marohnić <janko.marohnic@gmail.com>
2020-11-03 08:06:03 +0100

    Move rodauth_app.rb to app/lib/

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,6 @@ module RodauthDemo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # autoload files in the lib/ directory
-    config.autoload_paths += %W[#{config.root}/lib]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
