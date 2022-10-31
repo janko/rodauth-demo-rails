88a7c2f10aab45a8c5a58d7bdb99f0cf39f1e86a
Janko Marohnić <janko.marohnic@gmail.com>
2022-10-31 17:43:10 +0100

    Explicitly define a `#current_account` method
    This is now the recommended way, as the built-in `#current_account`
    controller helper has been soft-deprecated.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   private
 
+  def current_account
+    rodauth.rails_account
+  end
+  helper_method :current_account
+
   def authenticate
     rodauth.require_account
   end
