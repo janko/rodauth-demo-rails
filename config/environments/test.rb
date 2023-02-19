fa879a66750a7e84c0a25148221375a010542270
Janko Marohnić <janko.marohnic@gmail.com>
2023-02-19 18:48:43 +0100

    Switch back to `config.cache_classes`
    The `config.enable_reloading` setting will exist only in Rails 7.1. It
    seems that Rails allows you to assign anything to `config`, that's why
    it didn't complain when I used `config.enable_reloading` in Rails 7.0.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ require "active_support/core_ext/numeric/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.enable_reloading = false
+  config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
