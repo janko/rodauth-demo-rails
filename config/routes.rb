f753ad7fa9f32f37388d4b5fc3d2a10e2ca63364
Janko Marohnić <janko.marohnic@gmail.com>
2023-08-04 00:11:15 +0200

    Remove unused OmniAuth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "home#index"
   resources :posts
-  get "/auth/:provider/callback", to: "rodauth#omniauth"
 
   namespace :admin do
     root to: "home#index"
