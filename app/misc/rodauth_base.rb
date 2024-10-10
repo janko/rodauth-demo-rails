0199402f53294710fd2bff798297e0bb33feab47
Janko Marohnić <janko.marohnic@gmail.com>
2024-10-10 22:33:23 +0200

    Disable `webauthn_autofill` feature
    It now requires closing three windows to refuse automatic passkey login
    on Chrome (1Password, native, alternative).

--- app/misc/rodauth_base.rb
+++ app/misc/rodauth_base.rb
@@ -6,7 +6,7 @@ class RodauthBase < Rodauth::Rails::Auth
       :reset_password, :change_password, :change_password_notify,
       :change_login, :verify_login_change,
       :otp, :sms_codes, :recovery_codes,
-      :webauthn, :webauthn_login, :webauthn_autofill,
+      :webauthn, :webauthn_login,
       :close_account, :argon2, :omniauth
 
     # Initialize Sequel and have it reuse Active Record's database connection.
